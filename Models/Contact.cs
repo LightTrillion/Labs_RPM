b987e0c8b9cc63983232df892cc96cbf43e497ec
LightTrillion <borlor231@gmail.com>
2026-05-07 03:44:35 +0700

    Лабораторная работа 9. Конец 060526

---
+++ Models/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using laba_9_MVVM.ViewModels;
+
+namespace laba_9_MVVM.Models
+{
+    public class Contact : ObservableObject
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public Contact(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+
+        public bool Validate()
+        {
+
+            bool isNameValid = !string.IsNullOrWhiteSpace(Name);
+
+            bool isPhoneValid = !string.IsNullOrWhiteSpace(Phone) &&
+                ((Phone.StartsWith("+7") && Phone.Length == 12) ||
+                (Phone.Length == 10));
+
+            return (isNameValid && isPhoneValid);
+        }
+    }
+}
